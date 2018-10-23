2da8ea9acc2906a5b04ff51c23b08e924f82bb14
Куштанов Руслан Камилевич <kushtush@mail.ru>
2018-10-23 20:20:01 +0300

    refactor

--- ReactionsModule/ReactionsModule/module.js
+++ ReactionsModule/ReactionsModule/module.js
@@ -5,12 +5,21 @@ var Module;
     var EMOJI_STYLE = "\n\tflex:1 1 auto;\n\tbackground:  no-repeat;\n\tbackground-size: cover 100% 100%;\n\tbackround-origin:content-box;\n\tbackground-size:contain;\n\tborder:1px solid";
     var INNERSPAN_STYLE = "\n\torder:-1;\n\tmargin:5%;\n\tfont-size:4ex;";
     var SmileStorage = /** @class */ (function () {
+        /**
+            *Create Storage
+            *@param {object} Storage -Using storage
+            *@param {function} saveFunc - custom function on save counter
+            */
         function SmileStorage(Storage, saveFunc) {
             this.Storage = Storage;
             if (saveFunc !== undefined) {
                 this.setItem = saveFunc;
             }
         }
+        /**
+         * Get value by key
+         * @param {string | number} key - key of storage
+         */
         SmileStorage.prototype.getItem = function (key) {
             if ("getItem" in Storage) {
                 var result = this.Storage.getItem("" + key);
@@ -19,10 +28,15 @@ var Module;
                 result = this.Storage[key];
             }
             if (result === null) {
-                result = undefined;
+                result = "undefined";
             }
             return result;
         };
+        /**
+         * Set value by key
+         * @param key - key of storage
+         * @param value - setting value
+         */
         SmileStorage.prototype.setItem = function (key, value) {
             if ("setItem" in Storage) {
                 this.Storage.setItem("" + key, String(value));
@@ -33,109 +47,152 @@ var Module;
         };
         return SmileStorage;
     }());
-    var Render;
-    (function (Render) {
-        var selectNumber = localStorage.getItem("selected") === null ?
-            Infinity : +localStorage.getItem("selected");
-        function render(config, onSelect) {
+    var Render = /** @class */ (function () {
+        /**
+             * Create elements for module
+             * @param {Iconfig} config - Module parameters
+             * @param {function} onContainerClick - use on container click
+             */
+        function Render(config, onContainerClick) {
+            var _this = this;
+            this.containers = [];
+            this.Storage = new SmileStorage(localStorage);
             if (config.Storage) {
-                Storage = new SmileStorage(config.Storage, config.saveFunc);
+                this.Storage = new SmileStorage(config.Storage, config.saveFunc);
             }
-            _filler = CreateFiller();
-            CreateText(config.text);
-            CreateCounters(config, onSelect) // create array of containers
-                .forEach(function (elem) { return _filler.appendChild(elem); }); // Insert containers into filler
-            config.RootElement.appendChild(_filler);
+            this._holder = this.CreateHolder();
+            this.CreateText(config.text);
+            this.CreateCounters(config, onContainerClick) // Create array of containers
+                .forEach(function (elem) { return _this._holder.appendChild(elem); }); // Insert containers into filler
+            config.RootElement.appendChild(this._holder);
         }
-        Render.render = render;
-        var _filler;
-        var previousNumber;
-        var Storage = new SmileStorage(localStorage);
-        var containers = [];
-        function CreateCounters(config, onSelect) {
+        /**
+         * Create counter container
+         * @param {number} i - number of container
+         * @param {Function} onContainerClick - use on click
+         */
+        Render.prototype.CreateContainer = function (i, onContainerClick) {
+            var container = document.createElement("div");
+            container.style.cssText = EMOJI_CONTAINER_STYLE;
+            container.addEventListener("click", function (e) {
+                onContainerClick(i);
+            }.bind(this));
+            return container;
+        };
+        Render.prototype.CreateCounters = function (config, onContainerClick) {
             for (var i = 0; i < config.arrayEmoji.length; i++) {
                 var emot = config.arrayEmoji[i];
                 var MAX_EMOT_LENGTH = 3;
                 if (emot.length < MAX_EMOT_LENGTH) {
-                    var container = CreateContainer(i, onSelect);
-                    var emoji = CreateEmoji(i, emot);
-                    containers.push(container);
+                    var container = this.CreateContainer(i, onContainerClick);
+                    var emoji = this.CreateEmoji(i, emot);
+                    this.containers.push(container);
                     container.appendChild(emoji);
                 }
             }
-            return containers;
-        }
-        function CreateEmoji(i, emot) {
+            return this.containers;
+        };
+        /**
+         * Create elem with emotion
+         * @param {number} i - Number of container
+         * @param {string} emot - Emotion symbol
+         */
+        Render.prototype.CreateEmoji = function (i, emot) {
             var emoji = document.createElement("div");
-            emoji.textContent = Storage.getItem(String(i));
+            var count = this.Storage.getItem(String(i)) === undefined ? "0" : this.Storage.getItem(String(i));
+            emoji.textContent = count;
             emoji.style.cssText = EMOJI_STYLE;
             var code = (emot.codePointAt(0)).toString(16);
             emoji.style.backgroundImage = "url(https://badoocdn.com/big/chat/emoji@x2/" + code + ".png)";
             return emoji;
-        }
-        function CreateContainer(i, onSelect) {
-            var container = document.createElement("div");
-            container.style.cssText = EMOJI_CONTAINER_STYLE;
-            if (i === selectNumber) {
-                container.style.backgroundColor = "pink";
-                previousNumber = selectNumber;
-            }
-            if (Storage.getItem(String(i)) === undefined) {
-                Storage.setItem(String(i), "0");
-            }
-            container.addEventListener("click", function (e) {
-                onContainerClick(i, onSelect, containers);
-            }.bind(this));
-            return container;
-        }
-        function CreateFiller() {
+        };
+        /**
+         * Create main element
+         */
+        Render.prototype.CreateHolder = function () {
             var filler = document.createElement("div");
             filler.style.cssText = EMOJI_FILLER_STYLE;
             return filler;
-        }
-        function onContainerClick(i, onSelect, containers) {
-            selectNumber = i;
-            onSelect(selectNumber, previousNumber, containers, Storage);
-            if (previousNumber !== undefined) {
-                containers[previousNumber].style.backgroundColor = "";
-            }
-            if (previousNumber === selectNumber) {
-                previousNumber = undefined;
-                localStorage.setItem("selected", "undefiend");
-            }
-            else {
-                containers[selectNumber].style.backgroundColor = "pink";
-                previousNumber = selectNumber;
-                localStorage.setItem("selected", "" + selectNumber);
-            }
-        }
-        // Create Text element and insert it
-        function CreateText(text) {
+        };
+        /**
+         * Create Text element and insert it
+         * @param {string} text - Needed text
+         */
+        Render.prototype.CreateText = function (text) {
             var span = document.createElement("span");
             span.style.cssText = INNERSPAN_STYLE;
             if (text) {
                 span.textContent = text;
             }
-            _filler.appendChild(span);
-        }
-    })(Render || (Render = {}));
+            this._holder.appendChild(span);
+        };
+        return Render;
+    }());
     var ReactionsModule = /** @class */ (function () {
+        /**
+         * Create Module
+         * @param config - Module parameters
+         */
         function ReactionsModule(config) {
-            // Work with storage
-            this.onSelect = function (selectNum, previousNumber, containers, storage) {
-                if (previousNumber !== undefined) {
-                    storage.setItem(String(previousNumber), String(+storage.getItem(String(previousNumber)) - 1));
-                    containers[previousNumber].children[0].textContent = storage.getItem(String(previousNumber));
-                }
-                if (previousNumber === selectNum) {
-                    return;
-                }
-                storage.setItem(String(selectNum), String(+storage.getItem(String(selectNum)) + 1));
-                containers[selectNum].children[0].textContent = storage.getItem(String(selectNum));
-            };
+            this.selectNumber = localStorage.getItem("selected") === null ?
+                Infinity : +localStorage.getItem("selected");
+            this.Storage = new SmileStorage(localStorage);
+            this.onContainerClick = this.onContainerClick.bind(this);
             this.RootElement = config.RootElement;
-            Render.render(config, this.onSelect);
+            if (config.Storage) {
+                this.Storage = new SmileStorage(config.Storage, config.saveFunc);
+            }
+            this.Rendering = new Render(config, this.onContainerClick);
+            this.setValues();
         }
+        /**
+         * Work with Storage
+         * @param {number} selectNum
+         * @param {number} previousNumber
+         * @param {container[]} containers
+         * @param {SmileStorage} storage
+         */
+        ReactionsModule.prototype.onSelect = function (selectNum, previousNumber, containers, storage) {
+            if (previousNumber !== undefined) {
+                var count_1 = this.Storage.getItem(String(previousNumber)) === undefined ? 0
+                    : +this.Storage.getItem(String(previousNumber));
+                storage.setItem(String(previousNumber), String(+count_1 - 1));
+                containers[previousNumber].children[0].textContent = storage.getItem(String(previousNumber));
+            }
+            if (previousNumber === selectNum) {
+                return;
+            }
+            var count = this.Storage.getItem(String(selectNum)) === undefined ? 0
+                : +this.Storage.getItem(String(selectNum));
+            storage.setItem(String(selectNum), String(count + 1));
+            containers[selectNum].children[0].textContent = storage.getItem(String(selectNum));
+        };
+        /**
+         * Use on click
+         * @param {number} i number of clicked container
+         */
+        ReactionsModule.prototype.onContainerClick = function (i) {
+            this.selectNumber = i;
+            this.onSelect(this.selectNumber, this.previousNumber, this.Rendering.containers, this.Storage);
+            this.setValues();
+        };
+        /**
+             * Set previous number and shows selected counter
+             */
+        ReactionsModule.prototype.setValues = function () {
+            if (this.previousNumber !== undefined) {
+                this.Rendering.containers[this.previousNumber].style.backgroundColor = "";
+            }
+            if (this.previousNumber === this.selectNumber) {
+                this.previousNumber = undefined;
+                localStorage.setItem("selected", "undefiend");
+            }
+            else {
+                this.Rendering.containers[this.selectNumber].style.backgroundColor = "pink";
+                this.previousNumber = this.selectNumber;
+                localStorage.setItem("selected", "" + this.selectNumber);
+            }
+        };
         return ReactionsModule;
     }());
     Module.ReactionsModule = ReactionsModule;
--- ReactionsModule/ReactionsModule/module.ts
+++ ReactionsModule/ReactionsModule/module.ts
@@ -1,5 +1,5 @@
 namespace Module {
-    const EMOJI_FILLER_STYLE: string = `
+	const EMOJI_FILLER_STYLE: string = `
 	background-color:grey;
 	width:100%;
 	height: 100%;
@@ -8,154 +8,212 @@ namespace Module {
 	flex-wrap:wrap;
 	align-items:center;
 	`;
-    const EMOJI_CONTAINER_STYLE: string = `
+	const EMOJI_CONTAINER_STYLE: string = `
 	height:100%;
 	flex:1 1 auto;
 	display:flex;
 	align-items:stretch`;
-    const EMOJI_STYLE: string = `
+	const EMOJI_STYLE: string = `
 	flex:1 1 auto;
 	background:  no-repeat;
 	background-size: cover 100% 100%;
 	backround-origin:content-box;
 	background-size:contain;
 	border:1px solid`;
-    const INNERSPAN_STYLE: string = `
+	const INNERSPAN_STYLE: string = `
 	order:-1;
 	margin:5%;
 	font-size:4ex;`;
-    type container = HTMLElement & { smileNumber?: number };
-    type onSelectType = (x: number, d: number, c: container[], storage: SmileStorage) => void;
-    type newString = string & { codePointAt?(x: number): number };
-    interface Iconfig {
-        arrayEmoji: newString[];
-        RootElement: HTMLElement;
-        Storage?: Storage | number[] | object;
-        text: string;
-        saveFunc?(key: string | number, value: string | number): void;
-    }
-    class SmileStorage {
-        public constructor(private Storage: Storage | number[] | object,
-            saveFunc?: (key: string | number, value: string | number) => void) {
-            if (saveFunc !== undefined) { this.setItem = saveFunc; }
-        }
-        public getItem(key: string | number): string {
-            if ("getItem" in Storage) { var result: string = (this.Storage as Storage).getItem(`${key}`); }
-            else { result = this.Storage[key]; }
-            if (result === null) { result = undefined; }
-            return result;
-        }
-        public setItem(key: string | number, value: string | number): void {
-            if ("setItem" in Storage) { (this.Storage as Storage).setItem(`${key}`, String(value)); }
-            else { this.Storage[key] = String(value); }
-        }
-    }
-    namespace Render {
-        let selectNumber: number | undefined = localStorage.getItem("selected") === null ?
-            Infinity : +localStorage.getItem("selected");
-		export function render(config: Iconfig, onSelect: onSelectType): void {
-            if (config.Storage) { Storage = new SmileStorage(config.Storage, config.saveFunc); }
-            _filler = CreateFiller();
-            CreateText(config.text);
-            CreateCounters(config, onSelect)// create array of containers
-                .forEach((elem: HTMLElement) => _filler.appendChild(elem));  // Insert containers into filler
-            config.RootElement.appendChild(_filler);
-        }
-        let _filler: HTMLElement;
-        let previousNumber: number | undefined;
-		let Storage: SmileStorage = new SmileStorage(localStorage);
-		const containers: container[] = [];
-        function CreateCounters(config: Iconfig, onSelect: onSelectType): container[] {
+	type container = HTMLElement & { smileNumber?: number };
+	type onSelectType = (x: number, d: number, c: container[], storage: SmileStorage) => void;
+	type newString = string & { codePointAt?(x: number): number };
+	interface Iconfig {
+		arrayEmoji: newString[];
+		RootElement: HTMLElement;
+		Storage?: Storage | number[] | object;
+		text: string;
+		saveFunc?(key: string | number, value: string | number): void;
+	}
+	class SmileStorage {
 
-            for (let i: number = 0; i < config.arrayEmoji.length; i++) {
-                const emot: newString = config.arrayEmoji[i];
-                const MAX_EMOT_LENGTH: number = 3;
-				if (emot.length < MAX_EMOT_LENGTH) {
-					const container: container = CreateContainer(i, onSelect);
+		/**
+			*Create Storage
+			*@param {object} Storage -Using storage
+			*@param {function} saveFunc - custom function on save counter
+			*/
+		public constructor(private Storage: Storage | number[] | object,
+			saveFunc?: (key: string | number, value: string | number) => void) {
+			if (saveFunc !== undefined) { this.setItem = saveFunc; }
+		}
+		/**
+		 * Get value by key
+		 * @param {string | number} key - key of storage
+		 */
+		public getItem(key: string | number): string {
+			if ("getItem" in Storage) { var result: string = (this.Storage as Storage).getItem(`${key}`); }
+			else { result = this.Storage[key]; }
+			if (result === null) { result = "undefined"; }
+			return result;
+		}
+		/**
+		 * Set value by key
+		 * @param key - key of storage
+		 * @param value - setting value
+		 */
+		public setItem(key: string | number, value: string | number): void {
+			if ("setItem" in Storage) { (this.Storage as Storage).setItem(`${key}`, String(value)); }
+			else { this.Storage[key] = String(value); }
+		}
+	}
+	class Render {
+		public containers: container[] = [];
+		private _holder: HTMLElement;
 
-                        const emoji: HTMLElement = CreateEmoji(i, emot);
-                        containers.push(container);
-                        container.appendChild(emoji);
-                    }
-            }
-            return containers;
-        }
-        function CreateEmoji(i: number, emot: newString): HTMLElement {
-            const emoji: HTMLElement = document.createElement("div");
-            emoji.textContent = Storage.getItem(String(i));
-            emoji.style.cssText = EMOJI_STYLE;
-            const code: string = (emot.codePointAt(0)).toString(16);
-            emoji.style.backgroundImage = `url(https://badoocdn.com/big/chat/emoji@x2/${code}.png)`;
-            return emoji;
-        }
+		private Storage: SmileStorage = new SmileStorage(localStorage);
+		/**
+			 * Create elements for module
+			 * @param {Iconfig} config - Module parameters
+			 * @param {function} onContainerClick - use on container click
+			 */
+		public constructor(config: Iconfig, onContainerClick: (i: number) => void) {
+			if (config.Storage) { this.Storage = new SmileStorage(config.Storage, config.saveFunc); }
+			this._holder = this.CreateHolder();
+			this.CreateText(config.text);
+			this.CreateCounters(config, onContainerClick)// Create array of containers
+				.forEach((elem: HTMLElement) => this._holder.appendChild(elem));  // Insert containers into filler
+			config.RootElement.appendChild(this._holder);
+		}
+		/**
+		 * Create counter container
+		 * @param {number} i - number of container
+		 * @param {Function} onContainerClick - use on click
+		 */
+		private CreateContainer(i: number, onContainerClick: (i: number) => void): HTMLElement {
+			const container: container = document.createElement("div");
+			container.style.cssText = EMOJI_CONTAINER_STYLE;
 
-		function CreateContainer(i: number, onSelect: onSelectType): HTMLElement {
-            const container: container = document.createElement("div");
-            container.style.cssText = EMOJI_CONTAINER_STYLE;
-            if (i === selectNumber) {
-                container.style.backgroundColor = "pink";
-                previousNumber = selectNumber;
-            }
-            if (Storage.getItem(String(i)) === undefined) {
-                Storage.setItem(String(i), "0");
-			}
-			container.addEventListener("click", function (e: MouseEvent): void {
-				onContainerClick(i, onSelect, containers);
+			container.addEventListener("click", function(e: MouseEvent): void {
+				onContainerClick(i);
 			}.bind(this));
-            return container;
-        }
+			return container;
+		}
+		private CreateCounters(config: Iconfig, onContainerClick: (i: number) => void): container[] {
 
-        function CreateFiller(): HTMLElement {
-            const filler: HTMLElement = document.createElement("div");
-            filler.style.cssText = EMOJI_FILLER_STYLE;
-            return filler;
-        }
+			for (let i: number = 0; i < config.arrayEmoji.length; i++) {
+				const emot: newString = config.arrayEmoji[i];
+				const MAX_EMOT_LENGTH: number = 3;
+				if (emot.length < MAX_EMOT_LENGTH) {
+					const container: container = this.CreateContainer(i, onContainerClick);
 
-        function onContainerClick(i: number, onSelect: onSelectType,
-            containers: container[]): void {
-            selectNumber = i;
-            onSelect(selectNumber, previousNumber, containers, Storage);
-            if (previousNumber !== undefined) {
-                containers[previousNumber].style.backgroundColor = "";
-            }
-            if (previousNumber === selectNumber) {
-                previousNumber = undefined;
-                localStorage.setItem("selected", "undefiend");
-            } else {
-                containers[selectNumber].style.backgroundColor = "pink";
-                previousNumber = selectNumber;
-                localStorage.setItem("selected", `${selectNumber}`);
-            }
-        }
-        // Create Text element and insert it
-        function CreateText(text: string): void {
-            const span: HTMLElement = document.createElement("span");
-            span.style.cssText = INNERSPAN_STYLE;
-            if (text) {
-            span.textContent = text;
-        }
-            _filler.appendChild(span);
-        }
-    }
-    export class ReactionsModule {
-        public RootElement: HTMLElement;
-        public constructor(config: Iconfig) {
-            this.RootElement = config.RootElement;
-            Render.render(config, this.onSelect);
-        }
-        // Work with storage
-        public onSelect: onSelectType =
-            function(selectNum: number, previousNumber: number, containers: container[], storage: SmileStorage): void {
-             if (previousNumber !== undefined) {
-                storage.setItem(String(previousNumber), String(+storage.getItem(String(previousNumber)) - 1));
-                containers[previousNumber].children[0].textContent = storage.getItem(String(previousNumber));
-            }
-             if (previousNumber === selectNum) {
-                return;
-            }
-             storage.setItem(String(selectNum), String(+storage.getItem(String(selectNum)) + 1));
-             containers[selectNum].children[0].textContent = storage.getItem(String(selectNum));
-        };
+					const emoji: HTMLElement = this.CreateEmoji(i, emot);
+					this.containers.push(container);
+					container.appendChild(emoji);
+					}
+			}
+			return this.containers;
+		}
+		/**
+		 * Create elem with emotion
+		 * @param {number} i - Number of container
+		 * @param {string} emot - Emotion symbol
+		 */
+		private CreateEmoji(i: number, emot: newString): HTMLElement {
+			const emoji: HTMLElement = document.createElement("div");
+			let count: string = this.Storage.getItem(String(i)) === undefined ? "0" : this.Storage.getItem(String(i));
+			emoji.textContent = count;
+			emoji.style.cssText = EMOJI_STYLE;
+			const code: string = (emot.codePointAt(0)).toString(16);
+			emoji.style.backgroundImage = `url(https://badoocdn.com/big/chat/emoji@x2/${code}.png)`;
+			return emoji;
+		}
+		/**
+		 * Create main element
+		 */
+		private CreateHolder(): HTMLElement {
+			const filler: HTMLElement = document.createElement("div");
+			filler.style.cssText = EMOJI_FILLER_STYLE;
+			return filler;
+		}
+		/**
+		 * Create Text element and insert it
+		 * @param {string} text - Needed text
+		 */
+		private CreateText(text: string): void {
+			const span: HTMLElement = document.createElement("span");
+			span.style.cssText = INNERSPAN_STYLE;
+			if (text) {
+			span.textContent = text;
+		}
+			this._holder.appendChild(span);
+		}
 
-    }
+	}
+	export class ReactionsModule {
+
+		public RootElement: HTMLElement;
+		private previousNumber: number | undefined;
+		private Rendering: Render;
+		private selectNumber: number | undefined = localStorage.getItem("selected") === null ?
+			Infinity : +localStorage.getItem("selected");
+		private Storage: SmileStorage = new SmileStorage(localStorage);
+		/**
+		 * Create Module
+		 * @param config - Module parameters
+		 */
+		public constructor(config: Iconfig) {
+			this.onContainerClick = this.onContainerClick.bind(this);
+			this.RootElement = config.RootElement;
+			if (config.Storage) { this.Storage = new SmileStorage(config.Storage, config.saveFunc); }
+			this.Rendering = new Render(config, this.onContainerClick);
+			this.setValues();
+		}
+			/**
+			 * Work with Storage
+			 * @param {number} selectNum
+			 * @param {number} previousNumber
+			 * @param {container[]} containers
+			 * @param {SmileStorage} storage
+			 */
+		public onSelect(selectNum: number, previousNumber: number, containers: container[], storage: SmileStorage): void {
+			if (previousNumber !== undefined) {
+				let count: number = this.Storage.getItem(String(previousNumber)) === undefined ? 0
+					: +this.Storage.getItem(String(previousNumber));
+				storage.setItem(String(previousNumber), String(+count - 1));
+				containers[previousNumber].children[0].textContent = storage.getItem(String(previousNumber));
+			}
+			if (previousNumber === selectNum) {
+				return;
+			}
+			let count: number = this.Storage.getItem(String(selectNum)) === undefined ? 0
+				: +this.Storage.getItem(String(selectNum));
+			storage.setItem(String(selectNum), String(count + 1));
+			containers[selectNum].children[0].textContent = storage.getItem(String(selectNum));
+		}
+		/**
+		 * Use on click
+		 * @param {number} i number of clicked container
+		 */
+		private onContainerClick(i: number): void {
+			this.selectNumber = i;
+			this.onSelect(this.selectNumber, this.previousNumber, this.Rendering.containers, this.Storage);
+			this.setValues();
+		}
+		/**
+			 * Set previous number and shows selected counter
+			 */
+		private setValues(): void {
+			if (this.previousNumber !== undefined) {
+				this.Rendering.containers[this.previousNumber].style.backgroundColor = "";
+			}
+			if (this.previousNumber === this.selectNumber) {
+				this.previousNumber = undefined;
+				localStorage.setItem("selected", "undefiend");
+			}
+			else {
+				this.Rendering.containers[this.selectNumber].style.backgroundColor = "pink";
+				this.previousNumber = this.selectNumber;
+				localStorage.setItem("selected", `${this.selectNumber}`);
+			}
+		}
+	}
 }
